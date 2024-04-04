b1b00e002d5504218b0ead35b197db5c257d5865
ewangclarkson <ewangclarks@gmail.com>
2024-04-04 20:49:33 +0100

    package service need no socker comm

--- package-service/app.js
+++ package-service/app.js
@@ -3,12 +3,8 @@ const app = express();
 const logger = require('./logging/logger');
 const config = require('config');
 const db = require('./startup/database');
-const http = require('http');
-
-const {Server} = require('socket.io');
 
 
-let io;
 
 const startServer = function () {
 
@@ -16,20 +12,16 @@ const startServer = function () {
         .then(() => console.log(`You have successfully connected to database`))
         .catch(() => new Error("Failed to connect to mongoDB"));
 
-    const server = http.createServer(app);
 
-    io = new Server(server);
-
-    require('./sockets/package.sockets')(io);
     require('./startup/boot')(app);
 
     process.on("uncaughtException",
         (exp) => logger.error(exp.message, exp));
 
     const port = config.get("app.port");
-    return server.listen(port, () => console.log(`Listening ${config.get('name')} on port ${port}`));
+    return app.listen(port, () => console.log(`Listening ${config.get('name')} on port ${port}`));
 };
 
 const server = startServer();
 
-module.exports = {server, io};
+module.exports = server;
--- package-service/sockets/package.socket..js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = function(io){
-    io.on('connection',(socket)=>{
-
-    });
-};
