96696aaa0a94d703f941cbbf77859d50d1b0ad70
ewangclarkson <ewangclarks@gmail.com>
2024-04-18 08:40:39 +0100

    fix bug

--- user-service/middleware/error.handler.js
+++ user-service/middleware/error.handler.js
@@ -3,6 +3,5 @@ const HttpStatus = require('pk-common-lib');
 
 module.exports = function (error, req, res, next) {
     winston.error(error.message, error);
-    console.log(error)
     return res.status(HttpStatus.INTERNAL_SERVER).send('Something failed')
 };
